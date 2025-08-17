1a2f802bfea08006236164bd9390eaab8779d04e
Elnuget <cangulo009@outlook.es>
2025-08-17 14:44:31 -0500

    navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,7 +1,10 @@
 <!-- Navbar -->
 <nav id="navBar" class="navbar navbar-expand-lg sticky-top text-uppercase" data-aos="fade-up">
     <div class="container">
-        <a class="navbar-brand fw-bold" href="#">Rotto Tattoo Studio</a>
+        <a class="navbar-brand fw-bold" href="#" style="line-height: 0.9; display: flex; flex-direction: column; align-items: flex-start;">
+            <span style="font-size: 1.1rem; letter-spacing: 1px;">ROTTO</span>
+            <span style="font-size: 0.8rem; letter-spacing: 0.5px;">TATTOO STUDIO</span>
+        </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" 
                 aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
